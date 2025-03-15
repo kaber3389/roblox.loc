98aa9ee1aca8fe018a6a8becb156155bb07cec23
Nikolay <kaber3389@gmail.com>
2025-03-15 23:52:58 +0300

    FR-1: добавил tw, страницы sign-in, sign-up

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,29 @@
 <template>
-    <header>
-        Header
-    </header>
-    <slot />
+    <div class="container mx-auto px-[15%]">
+        <header>
+            <ul class="flex flex-row gap-3">
+                <li :class="{'active': route.path === '/'}" class="font-bold">
+                    <NuxtLink to="/">Главная</NuxtLink>
+                </li>
+                <li :class="{'active': route.path === '/sign-in'}" class="font-bold">
+                    <NuxtLink to="/sign-in">Вход</NuxtLink>
+                </li>
+                <li :class="{'active': route.path === '/sign-up'}" class="font-bold">
+                    <NuxtLink to="/sign-up">Регистрация</NuxtLink>
+                </li>
+            </ul>
+        </header>
+        <slot />
+    </div>
 </template>
+
+<script setup>
+const route = useRoute();
+console.log(route)
+</script>
+
+<style scoped>
+ul li.active a {
+    text-decoration: underline;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  css: ['./assets/css/app.css'],
   modules: ['@nuxt/image', '@nuxt/scripts', '@nuxt/ui']
 })
