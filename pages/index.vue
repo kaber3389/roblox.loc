9909372c1e66322ce945bddb77bc5ef23f58bdeb
Nikolay <kaber3389@gmail.com>
2025-03-16 03:28:10 +0300

    FR-3: get users

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,8 @@
 <template>
     <h1>Index</h1>
+    <pre>{{users}}</pre>
 </template>
+
+<script setup>
+const users = await $fetch('/api/user-index');
+</script>
---
+++ server/api/user-index.get.js
@@ -0,0 +1,14 @@
+export default defineEventHandler(async (event) => {
+    const runtimeConfig = useRuntimeConfig();
+
+    const response = await $fetch(`${runtimeConfig.public.apiBase}/user/index?apiKey=${runtimeConfig.server.apiKey}`, {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    });
+
+    return {
+        data: response,
+    };
+});
